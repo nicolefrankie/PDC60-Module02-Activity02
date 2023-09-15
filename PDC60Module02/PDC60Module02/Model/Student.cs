4256865641dae710c1307793e8b062c2bd4a51c8
Nicole Frankie Capuno <colecapuno@gmail.com>
2023-09-15 11:21:58 +0800

    first commit

---
+++ PDC60Module02/PDC60Module02/Model/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDC60Module02.Model
+{
+    class Student
+    {
+        public string Subjectcode { get; set; }
+        public string SubjectTitle { get; set; }
+        public int Unit { get; set; }
+    }
+}
